7d99c9c28fb5efc498a1d358e748b18cdbbf029b
Ana <anamassaro02@gmail.com>
2020-09-24 15:16:01 -0300

    listagem dinamica

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
 import { Container } from './styles';
 
 const Content: React.FC = ({ children }) => {
+ 
   return (
     <Container>
       {children}
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,30 +1,114 @@
-import React from 'react';
-import { Container, Content } from './styles'
+import React, { useMemo, useState, useEffect } from 'react';
+import { Container, Content, Filters } from './styles'
 import ContentHeader from '../../components/ContentHeader'
 import SelectInput from '../../components/SelectInput'
 import HistoryFinanceCard from '../../components/HistoryFinanceCard'
 
-const List: React.FC = () => {
+import gains from '../../repositories/gains';
+import expenses from '../../repositories/expenses';
 
-  const options = [
-    {value: 'Rodrigo', label: 'Rodrigo'},
-    {value: 'Maria', label: 'Maria'},
-    {value: 'Ana', label: 'Ana'}
+
+interface IRouteParams {
+  match: {
+    params: {
+      type: string;
+    }
+  }
+}
+
+interface IData {
+  id: string;
+  description: string;
+  amountFormatted: string;
+  frequency: string;
+  dataFormatted: string;
+  tagColor: string;
+}
+const List: React.FC<IRouteParams> = ({ match }) => {
+
+  /*useState é um array, onde na primeira posição ele guarda o valor do estado,
+  e na segunda posição ele guarda uma função que atualiza o estado*/
+  const [data, setData] = useState<IData[]>([]);
+
+  const { type } = match.params;
+  const itens = useMemo(() => {
+    return type === 'entry-balance' ? {
+      title: 'Entradas',
+      lineColor: '#F7931B'
+    } : {
+      title: 'Saídas',
+      lineColor: '#E44C4E'
+    }
+  },[type]);
+
+
+  const listData = useMemo(() => {
+    return type === 'entry-balance' ? gains : expenses;
+  },[type]);
+
+  const months = [
+    {value: 7, label: 'Julho'},
+    {value: 8, label: 'Agosto'},
+    {value: 9, label: 'Setembro'}
   ]
 
+  const years = [
+    {value: 2020, label: 2020},
+    {value: 2019, label: 2019},
+    {value: 2018, label: 2018},
+  ]
+
+  useEffect(() => {
+
+    const response = listData.map(item => {
+      return {
+        id: String(Math.random() * data.length),
+        description: item.description,
+        amountFormatted: item.amount,
+        frequency: item.frequency,
+        dataFormatted: item.date,
+        tagColor: item.frequency === 'recorrente' ? '#4E41F0' : '#E44C4E'
+      }
+    });
+
+    setData(response);
+  },[]);
+  
   return (
     <Container>
-       <ContentHeader title="Saídas" lineColor="#E44C4E">
-        <SelectInput options={options}/>
+       <ContentHeader title={itens.title} lineColor={itens.lineColor}>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
 
+      <Filters>
+        <button 
+          type="button"
+          className="tag-filter tag-filter-recurrent"
+        >
+          Recorrentes
+        </button>
+
+        <button 
+          type="button"
+          className="tag-filter tag-filter-eventual"
+        >
+          Eventuais
+        </button>
+      </Filters>
+
       <Content>
-      <HistoryFinanceCard
-          tagColor="#E44C4E"
-          title="Conta de Luz"
-          subtitle="23/09/2020"
-          amount="R$ 130,00"
-      />
+        {
+          data.map(item => (
+            <HistoryFinanceCard
+              key={item.id}
+              tagColor={item.tagColor}
+              title={item.description}
+              subtitle={item.dataFormatted}
+              amount={item.amountFormatted}
+            />
+          ))
+        }
       </Content>
     </Container>
   );
